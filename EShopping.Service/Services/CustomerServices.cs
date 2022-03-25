d563570828d77c428d83c49069e9bcc2a835cb3f
Clever Coder <clevercoderr@gmail.com>
2022-03-25 15:41:23 +0500

    Add project files.

---
+++ EShopping.Service/Services/CustomerServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopping.Service.Services
+{
+    public class CustomerServices
+    {
+    }
+}
